df95a15e093dc047fcfbf2cfae9a418338f51c12
David <dmolina101@gmail.com>
2018-01-12 15:40:38 -0430

    Task #18

---
+++ test.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const log = require('./utils').log()
+
+log.info('hola2')
--- utils.js
+++ utils.js
@@ -6,9 +6,35 @@ module.exports.toTimeStampUnix = (dateUS) => {
   return moment(strDate.slice(0, 4) + '/' + strDate.slice(4, 6) + '/' + strDate.slice(6, 8), 'YYYY/M/D').unix()
 }
 
-module.exports.log= (text)=>{
-      const logger = require('./Logger')
-      //console.log(text)
-      logger.info(text)
+module.exports.log = () => {
+  const winston = require('winston')
+  const fs = require('fs')
+  const logDir = 'logs'
+  const tsFormat = () => (new Date()).toLocaleTimeString()
+
+  if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir)
+  }
+
+  const logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({
+        timestamp: tsFormat,
+        colorize: true,
+        level: 'info'
+      }),
+      new (require('winston-daily-rotate-file'))({
+        filename: `${logDir}/-info.log`,
+        timestamp: tsFormat,
+        datePattern: 'yyyy-MM-dd',
+        prepend: true,
+        colorize: true,
+        level: 'info'
+      })
+    ]
+  })
+  return {
+    info: (text) => {logger.info(text)},
+    error: (text) => {logger.error(text)}
+  }
 }
-    
